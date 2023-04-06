9a50051d0129eac7f7678bcf39c157c5c44e6b5f
Micael595 <112912271+Micael595@users.noreply.github.com>
2023-04-05 22:44:40 -0300

    nuevos cambios en creacion de de crear una lista  a diccionario

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 
 let pedido = prompt("Buenos dias cual va ser su pedido?  \n 1 simple con queso \n 2 doble con queso \n 3 doble roque \n 4 doble onion " )
 
-/* let burguerType1 = "simple con queso"
-let burguerType2 = "doble con queso"
-let burguerType3 = "doble roque"
-let burguerType4 = "doble onion" */
+let listaPedido = []
 
-function hamburguesa(carnes,quesos,precio,extra){
+function hamburguesa(nombre,carnes,quesos,precio,extra){
+    this.nombre = nombre
     this.carnes = carnes
     this.quesos = quesos
     this.precio = precio
     this.extra = extra
 }
-const simpleQueso = new hamburguesa("una carne","dos quesos",1600,"")
-const dobleQueso = new hamburguesa("dos carne","cuatro quesos",1800,"")
-const dobleRoque = new hamburguesa("dos carne", "queso roquefort",1850,"")
-const dobleOnion = new hamburguesa("dos carne","dos quesos",1900,"cebolla caramelizada")
+const simpleQueso = new hamburguesa("simple con queso","una carne","dos quesos",1600,"")
+const dobleQueso = new hamburguesa("doble con queso","dos carne","cuatro quesos",1800,"")
+const dobleRoque = new hamburguesa("doble roque","dos carne", "queso roquefort",1850,"")
+const dobleOnion = new hamburguesa("doble onion","dos carne","dos quesos",1900,"cebolla caramelizada")
 
 if (pedido == NaN ){
     alert("Alerta, ha ingresado un valor no numerico, por favor ingrese un numero segun la hamburguesa deseada")
@@ -29,30 +27,37 @@ function totalPrice(precio,cantidad){
 while (pedido != NaN) {
     if (pedido == 1) {
         let cantidad = prompt("cuantas simples con queso desea comprar? ")
-        
-        alert ("usted ha comprado " + cantidad + " " + "simple con queso" + "a $"+ totalPrice(simpleQueso.precio,Number(cantidad)) + ", Muchas gracias por su compra")
-
+        let precioTotal = totalPrice(simpleQueso.precio,Number(cantidad))
+        alert ("usted ha comprado " + cantidad + " " + "simple con queso" + "a $"+ precioTotal + ", Muchas gracias por su compra")
+        listaPedido.push({pedido:simpleQueso.nombre,precio:precioTotal})
     }else if (pedido == 2) {
-        let cantidad = parseInt(prompt("cuantas doble con queso desea comprar?"))
-        alert( "usted ha comprado " + cantidad + " " + "doble con Queso" + "a $" + totalPrice(dobleQueso.precio,Number(cantidad)) +", Muchas gracias por su compra")
+        let cantidad = prompt("cuantas doble con queso desea comprar?")
+        let precioTotal = totalPrice(dobleQueso.precio,Number(cantidad))
+        alert( "usted ha comprado " + cantidad + " " + "doble con Queso" + "a $" + precioTotal +", Muchas gracias por su compra")
+        listaPedido.push({pedido:dobleQueso.nombre,precio:precioTotal})
     }else if (pedido == 3) {
         let cantidad = prompt("cuantas doble roque desea comprar?")
-        alert( "usted ha comprado " + cantidad + " " + "doble Roque" + "a $" + totalPrice(dobleRoque.precio,Number(cantidad))  + ", Muchas gracias por su compra")
+        let precioTotal = totalPrice(dobleRoque.precio,Number(cantidad))
+        alert( "usted ha comprado " + cantidad + " " + "doble Roque" + "a $" + precioTotal  + ", Muchas gracias por su compra")
+        listaPedido.push({pedido:dobleRoque.nombre,precio:precioTotal})
     }else if (pedido == 4) {
         let cantidad = prompt("cuantas doble onion desea comprar?")
-        alert( "usted ha comprado " + cantidad + " " + "doble Onion" + "a $" +totalPrice(dobleOnion.precio,Number(cantidad)) +", Muchas gracias por su compra")
+        let precioTotal = totalPrice(dobleOnion.precio,Number(cantidad))
+        alert( "usted ha comprado " + cantidad + " " + "doble Onion" + "a $" + precioTotal +", Muchas gracias por su compra")
+        listaPedido.push({pedido:dobleOnion.nombre,precio:precioTotal})
     }
     else if (pedido == 0 || pedido >= 5)
     alert("Ha ingresado un numero fuera de las opciones disponibles, por favor reintentar ingresado 1,2,3 o 4")
     break
 }
 
+alert("usted tiene un pedido de: " + JSON.stringify(Object.values(listaPedido)))
 
 let nuevoPedido = prompt("Desea realizar otro pedido?, responder SI o NO" )
     if ((nuevoPedido == "SI")|| (nuevoPedido == "si")){
     location.reload()
     }
     else{
-        alert ("Gracias por comprar en BurguerLab")
-        location.reload()
+        alert("Gracias por su compra en Burgerlab")
+    location.reload()
     }
